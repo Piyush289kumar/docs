5b3456f0eb0770cc58352be8d783b6bb5d9b6168
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-04 13:18:49 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-900'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae labore laboriosam cumque iusto id placeat, vero temporibus nam eius aliquid exercitationem quo eaque at perferendis voluptas veritatis! Praesentium, mollitia quidem!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
