7fe13a533be335974aa035b119c655aa6859d39c
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-04 13:41:31 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import Background from './components/Background'
+import Foreground from './components/Foreground'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className='bg-red-900'>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae labore laboriosam cumque iusto id placeat, vero temporibus nam eius aliquid exercitationem quo eaque at perferendis voluptas veritatis! Praesentium, mollitia quidem!</p>
+      <div className='relative w-full h-screen bg-zinc-800'>
+        <Background />
+        <Foreground />
       </div>
     </>
   )
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='w-60 h-72 rounded-[30px] bg-zinc-900/90'>Card</div>
+  )
+}
+
+export default Card
