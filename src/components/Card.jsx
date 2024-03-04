aa73ed4f08edbd51dd91f7db96be5c85e9eafb34
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-04 15:07:47 +0530

    Build Finish

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaFileInvoice } from "react-icons/fa";
+import { MdCloudDownload } from "react-icons/md";
+import { IoMdCloseCircle } from "react-icons/io";
+import { motion } from "framer-motion"
 
-function Card() {
+function Card({ data, reference }) {
+
+
   return (
-    <div className='w-60 h-72 rounded-[30px] bg-zinc-900/90'>Card</div>
+    <motion.div drag whileDrag={{ scale: 1 }}  dragTransition={{ bounceStiffness: 200, bounceDamping: 100 }} dragConstraints={reference} className='relative w-60 h-72 rounded-[30px] bg-zinc-900/90 p-5 overflow-hidden cursor-pointer'>
+      <div className='absolute'>
+        <FaFileInvoice size="1rem" className='text-zinc-300' />
+        <p className='font-semibold mt-5 text-zinc-400 text-md leading-none tracking-tight'>{data.cardText}</p>
+      </div>
+      <div className='absolute bottom-0 left-0 w-full'>
+        <div className='px-5 mb-4 flex justify-between items-center'>
+          <p className='text-white font-mono font-semibold text-md'>{data.fileSize}</p>
+          {data.optionIcon ?
+            <MdCloudDownload size="1rem" className='text-zinc-300' />
+            : <IoMdCloseCircle size="1rem" className='text-zinc-300' />
+          }
+        </div>
+        {data.footer.isActive &&
+          <div className={`w-full py-2 ${data.footer.bgColor} text-center`}>
+            <h3 className='text-md font-semibold text-white'>{data.footer.footerText}</h3>
+          </div>
+          }
+
+      </div>
+    </motion.div>
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
 
 function Foreground() {
+
+    const ref = useRef(null)
+
+    const data = [
+        {
+            cardText: 'Lorem ipsum dolor sit amet consectetur.',
+            fileSize: '0.9md',
+            optionIcon: true,
+            footer: {
+                footerText: 'Download Now', isActive: true, bgColor: 'bg-green-600'
+            }
+        },
+        {
+            cardText: 'Lorem ipsum dolor sit amet consectetur.',
+            fileSize: '0.9md',
+            optionIcon: false,
+            footer: {
+                footerText: 'Upload', isActive: true, bgColor: 'bg-blue-600'
+            }
+        },
+        {
+            cardText: 'Lorem ipsum dolor sit amet consectetur.',
+            fileSize: '0.9md',
+            optionIcon: false,
+            footer: {
+                footerText: 'Download Now', isActive: false, bgColor: 'bg-green-600'
+            }
+        },
+    ]
+
+
     return (
         <>
-            <div className='fixed top-0 left-0 z-[3] w-full h-screen'>
-                <Card />
+            <div className='fixed top-0 left-0 z-[3] w-full h-screen py-12 px-16'>
+                <div ref={ref} className='mt-11 w-full h-[80vh] flex flex-wrap gap-10'>
+                    {
+                        data.map((item, idx) => (
+                            <Card data={item} key={idx} reference={ref} />
+                        ))
+                    }
+                </div>
+
             </div>
         </>
     )
